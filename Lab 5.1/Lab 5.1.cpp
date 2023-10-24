1ad2c6c78ef4c544eda774c02c7dc1e48073ee2d
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-10-24 22:53:43 +0300

    Add project files.

---
+++ Lab 5.1/Lab 5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double a, const double b, const double c); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double l = (f(1, t + s, s) + f(t, s * t, 1)) / (1 + pow(f(s, 1, t), 2));
+	cout << "l = " << l << endl;
+	return 0;
+}
+double f(const double a, const double b, const double c) // визначення
+{
+	return (a * sin(b) + b * sin(a) + pow(c, 2));
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.1/Lab 5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LAB51UnitTest
+{
+	TEST_CLASS(LAB51UnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = f(3, 2, 4);
+			Assert::AreEqual(t, 19.0101, 0.0001);
+		}
+	};
+}
